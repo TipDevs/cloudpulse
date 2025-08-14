b2bb81379f69340d8fb6091b2e5c9a9732bff0b4
TipDevs <tipdevs@gmail.com>
2025-08-14 22:29:03 +0100

    Module that handles event listeners

---
+++ src/modules/events.js
@@ -0,0 +1,43 @@
+import PubSub from "pubsub-js";
+import { retrieveCityInfo } from "./api";
+
+/*function that handles submitting userInput for 
+fetching weather data and also handles stopping form 
+element submitting data by preventing default behavior of form element */
+const queryEvent = (elements, callBack) => {
+  elements.queryInput.parentElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  elements.queryBtn.addEventListener("click", () => {
+    PubSub.publish("initial fresh fetching", elements);
+    callBack(elements.queryInput.value.trim());
+    elements.queryInput.value = "";
+    elements.queryInput.blur();
+  });
+};
+
+/*function that handles toggling temperature */
+const tempTogglingEvent = (elements) => {
+  elements.tempToggler.addEventListener("click", () => {
+    const celsiusIcon = elements.tempToggler.children[0];
+    const fahrenheitIcon = elements.tempToggler.children[1];
+    celsiusIcon.style.display =
+      celsiusIcon.style.display === "none" ? "block" : "none";
+    fahrenheitIcon.style.display =
+      fahrenheitIcon.style.display === "block" ? "none" : "block";
+    PubSub.publish("toggle temp");
+  });
+};
+// pubsub events
+PubSub.subscribe("domReady", (msg, elements) => {
+  PubSub.unsubscribe("queryEvent");
+  PubSub.unsubscribe("temp toggling event");
+  PubSub.subscribe("queryEvent", (msg, callBack) => {
+    queryEvent(elements, callBack);
+  });
+  PubSub.subscribe("temp toggling event", () => {
+    tempTogglingEvent(elements);
+  });
+  PubSub.publish("queryEvent", retrieveCityInfo);
+  PubSub.publish("temp toggling event");
+});
