9dc71d0a1250973e9c8eb8b90c9ff37488d4c717
TipDevs <tipdevs@gmail.com>
2025-08-14 21:51:49 +0100

    Add a function that weather fetches data from API or return an error if fetch failed

---
+++ src/modules/api.js
@@ -0,0 +1,47 @@
+// function to fetch data from Visual Crossing weather api
+const getWeather = async (location) => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}/?unitGroup=metric&key=NRSD4MPJ3AAY9JZ9QJMJ8W9T5`,
+      {
+        mode: "cors",
+      }
+    );
+    if (!response.ok) {
+      console.log(
+        `API request failed with status ${response.status} ${response.statusText}`
+      );
+    }
+    const weatherData = await response.json();
+    console.log(weatherData.timezone);
+    return {
+      temp: weatherData.currentConditions.temp,
+      windSpeed: weatherData.currentConditions.windspeed,
+      condition: weatherData.currentConditions.conditions,
+      humidity: weatherData.currentConditions.humidity,
+      location: weatherData.resolvedAddress,
+      description: weatherData.days[0].description,
+      cloudCover: weatherData.currentConditions.cloudcover,
+      visibility: weatherData.currentConditions.visibility,
+      timezone: weatherData.timezone,
+      error: null,
+    };
+  // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    return {
+      error: "Fetch failed, pls check location and try again",
+      temp: null,
+      windSpeed: null,
+      condition: null,
+      humidity: null,
+      description: null,
+      cloudCover: null,
+      visibility: null,
+      timezone: null,
+    };
+  }
+};
+
+
+
+
